8e78d0a99f6d87cb063c2db3b70d7fd6ea14a436
Marcos-Antoni <marcos7barrera@gmail.com>
2024-04-23 20:57:08 -0500

    add versel 0.1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +16,7 @@ export default defineConfig({
       }
     }
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel()
 });
